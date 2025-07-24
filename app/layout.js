cdfce1dcde61cee7ddb44c3c5f9907566b489bf4
Dhruv <dhruvkashyap458@gmail.com>
2025-07-24 21:01:52 +0530

    added dark theme

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
-        {children}
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main>{children}</main>
+        
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>started the project</div>
+    <>
+      <div>started the project</div>
+      <Button variant={"primary"}>clickkk</Button>
+    </>
   );
 }
