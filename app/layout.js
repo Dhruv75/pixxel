5aed23e8fae1781b9f84246182976471d5731cbd
Dhruv <dhruvkashyap458@gmail.com>
2025-07-24 21:16:27 +0530

    added parallex

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
+import { FloatingShapes } from "@/components/floating-shapes";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,16 +14,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-     <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <main>{children}</main>
-        
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+            <Toaster richColors/>
+            <FloatingShapes/>
+            {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
